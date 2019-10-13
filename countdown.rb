e8094bc509f1b57cdc6f032e5ef548e59278ac63
Michael Waldo <mbwcogic@yahoo.com>
2019-10-13 23:40:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,10 @@ while  seconds_to_midnight > 10
 end
 
 
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
+#def countdown_with_sleep(seconds_to_midnight)
+ #  puts "#{seconds_to_midnight} SECOND(S)!"
+  #  sleep(1)
+   # seconds_to_midnight -= 1
+  #end
+  #"HAPPY NEW YEAR!"
+#end
