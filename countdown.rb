06e049e19016bb58796aa291760121625819c589
Michael Waldo <mbwcogic@yahoo.com>
2019-10-13 23:44:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
  seconds_to_midnight = 10
 def countdown(seconds_to_midnight)
 while  seconds_to_midnight > 10
-   puts {seconds_to_midnight} SECOND(S)!" 
+   puts #{seconds_to_midnight} SECOND(S)!" 
    seconds_to_midnight -= 1
  end
  "HAPPY NEW YEAR!"
